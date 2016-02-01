70ac1d6fc67bf5188f5888c6c2f4855635ab3ecf
Stewart Birnam <stewart.birnam@autodesk.com>
2016-01-31 16:20:14 -0800

    tray delegates inital add

---
+++ python/tk_rv_cuts/tray_model.py
@@ -0,0 +1,20 @@
+
+
+# import the shotgun_model module from the shotgun utils framework
+import tank
+
+shotgun_model = tank.platform.import_framework("tk-framework-shotgunutils", "shotgun_model")
+SimpleShotgunModel = shotgun_model.SimpleShotgunModel
+ShotgunModel = shotgun_model.ShotgunModel
+
+class TrayModel(SimpleShotgunModel):
+
+        def __init__(self, parent, bg_task_manager=None):
+                SimpleShotgunModel.__init__(self, parent)
+
+        def load_data(self, entity_type, filters=None, fields=None, hierarchy=None):
+                filters = filters or []
+                fields = fields or ["code"]
+                hierarchy = hierarchy or [fields[0]]
+                ShotgunModel._load_data(self, entity_type, filters, hierarchy, fields)
+                self._refresh_data()
