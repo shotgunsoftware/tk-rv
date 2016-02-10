afa1692a31c2414edcd81a440c370c2b917b6649
Alan Trombla <alan@tweaksoftware.com>
2016-02-09 20:41:56 -0800

    shotgun_toolkit_mode: use session token from RV licensing
    * Retrieve the default Session token (and url/login) from the slutils module
      (Shotgun Licensing module) and use for Toolkit authentication.
    
    * Note additional slutils_py.py module needs to be installed alongside
      shotgun_toolkit_mode.py.

--- startup/shotgun_toolkit_mode.py
+++ startup/shotgun_toolkit_mode.py
@@ -1,9 +1,11 @@
+
 import os
 import re
 import sys
 import traceback
 
 from rv import rvtypes
+from rv import commands
 from PySide import QtGui
 
 # Define the integration mode name we'll be useing.
@@ -13,6 +15,70 @@ TK_RV_MODE_NAME = "tk_rv_mode"
 # up.
 # NOTE TO SELF: is this how we like it?
 os.environ["TK_RV_MODE_NAME"] = TK_RV_MODE_NAME
+
+# Assuming Toolkit is available in the path.
+from tank_vendor.shotgun_api3 import Shotgun
+from tank_vendor.shotgun_authentication.user import ShotgunUser
+from tank_vendor.shotgun_authentication.user_impl import ShotgunUserImpl
+import sgtk
+
+class RVUserImpl(ShotgunUserImpl):
+    """
+    A user that authenticates to the Shotgun server using a session token.
+    """
+
+    def __init__(self, host, login, session_token, http_proxy):
+        """
+        Constructor.
+
+        :param host: Host for this Shotgun user.
+        :param login: Login name for the user.
+        :param session_token: Session token for the user. If session token is None
+            the session token will be looked for in the users file.
+        :param http_proxy: HTTP proxy to use with this host. Defaults to None.
+
+        :raises IncompleteCredentials: If there is not enough values
+            provided to initialize the user, this exception will be thrown.
+        """
+        super(RVUserImpl, self).__init__(host, http_proxy)
+
+        self._login = login
+        self._session_token = session_token
+
+    def get_login(self):
+        """
+        Return the login name for this user.
+
+        :returns: The login name string.
+        """
+        return self._login
+
+    def create_sg_connection(self):
+        """
+        Create a Shotgun instance using the script user's credentials.
+
+        :returns: A Shotgun instance.
+        """
+        return Shotgun(
+            self.get_host(), session_token=self._session_token,
+            http_proxy=self.get_http_proxy()
+        )
+
+    def __repr__(self):
+        """
+        Return a string reprensentation of the user.
+
+        :returns: A string containing login and site.
+        """
+        return "<RVUser %s @ %s>" % (self._login, self._host)
+
+    def __str__(self):
+        """
+        Return the name of the user.
+
+        :returns: A string.
+        """
+        return self._login
 
 class ShotgunToolkit(rvtypes.MinorMode):
 
@@ -56,16 +122,36 @@ class ShotgunToolkit(rvtypes.MinorMode):
                 return engine
         else:
             import sgtk
-            import tank_vendor.shotgun_authentication
 
-            sa = tank_vendor.shotgun_authentication.ShotgunAuthenticator()
-            
-            # if you want to force a login, here's how.
+            # if you want to force a Toolkit-style login, here's how:
+            #
+            # import tank_vendor.shotgun_authentication
+            # sa = tank_vendor.shotgun_authentication.ShotgunAuthenticator()
             # sa.get_user_from_prompt()
-            
-            user = sa.get_user()
-            sg_conn = user.create_sg_connection()
-            sgtk.set_authenticated_user(user)
+            # user = sa.get_user()
+
+            try:
+                import slutils_py
+
+                # Get default session info from slutils (rv/shotgun licensing) module
+                (url, login, token) = slutils_py.defaultSession()
+
+                # XXX Need format of http_proxy arg to replace None below
+                user = ShotgunUser(RVUserImpl(url, login, token, None))
+                sg_conn = user.create_sg_connection()
+                sgtk.set_authenticated_user(user)
+
+                # XXX what if previous session is not the server we want ?
+                # XXX this forces restart if we fail here.  we instead should
+                #     wait for login process to succeed and then continue.
+
+            except:
+                commands.alertPanel (True, commands.ErrorAlert, "Login Session Invalid", 
+                        "Login with RV Shotgun session token failed; please use the File Menu's "
+                        "\"License Manager\" to log in to the Shotgun server and re-start RV.", 
+                        "Continue", None, None);
+                return None
+
             projectName = 'Big Buck Bunny'
             if os.environ.get('TANK_PROJECT_NAME'):
                 projectName = os.environ.get('TANK_PROJECT_NAME')
@@ -73,7 +159,6 @@ class ShotgunToolkit(rvtypes.MinorMode):
             sys.stderr.write("DEBUG: find project '%s'.\n" % projectName)
             project = sg_conn.find("Project", [["name", "is", projectName]])
             sys.stderr.write("DEBUG:     find result '%s'.\n" % str(project))
-
 
             tk = sgtk.sgtk_from_entity(project[0]["type"], project[0]["id"])
 
