4f8dd6bf6f5f4b457b380035493eec9895b09593
Jeff Beeland <jeff.beeland@autodesk.com>
2016-03-08 17:56:54 -0800

    #34904 SGTK as a plugin bootstrap, plus menu logic cleanup.
    The included rv_plugin_prototype subdirectory contains an RV package that provides a mode that handles bootstrapping toolkit in RV. It does this via the "toolkit as a plugin" work being developed as part of the v0.18 release of tk-core.
    
    Closes #1.

--- engine.py
+++ engine.py
@@ -8,43 +8,52 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-"""
-The OFFICIAL Toolkit engine for RV! 
-Accept no substitutes.
-"""
-
 import os
 import sys
+
 import tank
+import rv.qtutils
 
 from tank.platform import Engine
-from tank import TankError
-
-import rv.qtutils
-from PySide import QtCore
+from PySide import QtGui, QtCore
 
 class RVEngine(Engine):
+    """
+    Shotgun Toolkit engine for RV.
+    """
+
     #####################################################################################
     # Properties
 
     @property
+    def context_change_allowed(self):
+        """
+        Specifies that on-the-fly context changes are supported.
+        """
+        return True
+
+    @property
     def toolkit_rv_mode_name(self):
+        """
+        The name of the RV Mode that bootstrapped SGTK and started
+        the engine.
+        """
         return os.environ.get("TK_RV_MODE_NAME")
 
     @property
     def menu_name(self):
-        if self.get_setting("use_sgtk_as_menu_name"):
-            return "SGTK"
-        else:
-            return "tk-rv"
+        """
+        The name of the top-level menu created by the engine.
+        """
+        return "SG Review"
 
     #####################################################################################
     # Engine Initialization and Destruction
 
     def pre_app_init(self):
-        self.log_debug("%r: Initializing..." % self)
-        self.log_debug(os.environ.get("TANK_CONTEXT"))
-
+        """
+        Runs before apps have been initialized.
+        """
         # The assumption here is that the default case has us presenting a
         # Shotgun menu and loading it with at least Cutz Support and maybe
         # an expected Shotgun browser or two. _ui_enabled turns the menu on.
@@ -59,17 +68,40 @@ class RVEngine(Engine):
         QtCore.QTextCodec.setCodecForCStrings(utf8)
 
     def post_app_init(self):
+        """
+        Runs after all apps have been initialized. If running in a GUI
+        session of RV, the "SG Review" menu will be created and populated
+        with any app commands that have been registered with the engine.
+        """
         if self._ui_enabled:
             tk_rv = self.import_module("tk_rv")
             self._menu_generator = tk_rv.MenuGenerator(self)
             self._menu_generator.create_menu()
 
     def destroy_engine(self):
+        """
+        Tears down the engine and any menu items it has created.
+        """
         self.log_debug("%r: Destroying tk-rv engine." % self)
 
         if self._ui_enabled:
-            self.log_debug("%r: Destroying Shotgun menu" % self)
+            self.log_debug("%r: Destroying %s menu" % (self, self.menu_name))
             self._menu_generator.destroy_menu()
+
+    def post_context_change(self, old_context, new_context):
+        """
+        Run after any on-the-fly context changes. This will trigger a
+        rebuild of the top-level menu created by the engine when it was
+        initialized.
+
+        :param old_context: The previously-active context.
+        :param new_context: The context that was switched to.
+        """
+        if not self._ui_enabled:
+            return
+
+        self._menu_generator.destroy_menu()
+        self._menu_generator.create_menu()
 
     #####################################################################################
     # Logging
@@ -95,18 +127,34 @@ class RVEngine(Engine):
     # General Utilities
 
     def has_ui(self):
+        """
+        Whether RV is running in UI mode or not.
+        """
         return self._ui_enabled
 
     def get_dialog_parent(self):
+        """
+        The parent widget to use when creating top-level widgets for use
+        in RV.
+        """
         return rv.qtutils.sessionWindow()
 
     def get_gl_parent(self):
+        """
+        The RV session's GLView object.
+        """
         return rv.qtutils.sessionGLView()
 
     def get_top_toolbar(self):
+        """
+        RV's top toolbar widget.
+        """
         return rv.qtutils.sessionTopToolBar()
 
     def get_bottom_toolbar(self):
+        """
+        RV's bottom toolbar widget.
+        """
         return rv.qtutils.sessionBottomToolBar()
 
 
---
+++ rv_plugin_prototype/src/sgtk_bootstrap.py
@@ -0,0 +1,144 @@
+# Copyright (c) 2016 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import logging
+import cgi
+import sys
+import os
+
+import rv
+
+class ToolkitBootstrap(rv.rvtypes.MinorMode):
+    """
+    An RV mode that will handle bootstrapping SGTK and starting
+    up the tk-rv engine. The mode expects an installation of
+    tk-core to be present in the same directory underneath an
+    "sgtk_core" subdirectory. Alternatively, an environment variable
+    "TK_CORE" can be set to an alternate installation of tk-core
+    to override the default behavior.
+    """
+    def __init__(self):
+        """
+        Initializes the RV mode. An environment variable "TK_RV_MODE_NAME"
+        will be set to the name of this mode. That variable can then be
+        used by other logic to generate menu items in RV associated with
+        this mode.
+        """
+        super(ToolkitBootstrap, self).__init__()
+
+        self._mode_name = "sgtk_bootstrap"
+        self.init(self._mode_name, None, None)
+
+        # The menu generation code makes use of the TK_RV_MODE_NAME
+        # environment variable. Each menu that is created in RV is
+        # associated with a mode identified by its name. We need to
+        # make a note of our name as a result.
+        os.environ["TK_RV_MODE_NAME"] = self._mode_name
+
+    def activate(self):
+        """
+        Activates the RV mode and bootstraps SGTK.
+        """
+        rv.rvtypes.MinorMode.activate(self)
+
+        core = os.path.join(os.path.dirname(__file__), "sgtk_core")
+        core = os.environ.get("TK_CORE") or core
+
+        # append python path to get to the actual code
+        core = os.path.join(core, "python")
+
+        log.info("Looking for tk-core here: %s" % str(core))
+
+        # now we can kick off sgtk
+        sys.path.append(core)
+
+        # import bootstrapper
+        from tank_vendor import shotgun_base, shotgun_deploy, shotgun_authentication
+
+        # import authentication code
+        from sgtk_auth import get_toolkit_user
+
+        # bind toolkit logging to our logger
+        sgtk_root_logger = shotgun_base.get_sgtk_logger()
+        sgtk_root_logger.setLevel(logging.WARNING)
+        sgtk_root_logger.addHandler(log_handler)
+
+        # Get an authenticated user object from rv's security architecture
+        user = get_toolkit_user()
+        log.info("Will connect using %r" % user)
+
+        # Now do the bootstrap!
+        log.info("Ready for bootstrap!")
+        mgr = shotgun_deploy.ToolkitManager(user)
+
+        # Hint bootstrapper about our namespace so that we don't pick
+        # the site config for Maya or Desktop.
+        mgr.namespace = "rv"
+
+        mgr.base_configuration = dict(
+            # type="dev",
+            # path=r"d:\repositories\tk-config-rv",
+            path="git@github.com:shotgunsoftware/tk-config-rv.git",
+            type="git_branch",
+            branch="master",
+            version="latest",
+        )
+
+        # Bootstrap the tk-rv engine into an empty context!
+        mgr.bootstrap_engine("tk-rv")
+        log.info("Bootstrapping process complete!")
+
+
+    def deactivate(self):
+        """
+        Deactivates the mode and tears down the currently-running
+        SGTK engine.
+        """
+        import sgtk
+        rv.rvtypes.MinorMode.deactivate(self)
+
+        log.info("Shutting down engine...")
+
+        if sgtk.platform.current_engine():
+            sgtk.platform.current_engine().destroy()
+
+        log.info("Engine is down.")
+
+
+###############################################################################
+# functions
+
+def createMode():
+    """
+    Required to initialize the module. RV will call this function
+    to create your mode.
+    """
+    return ToolkitBootstrap()
+
+
+###############################################################################
+# logging
+
+log = logging.getLogger("sgtk_rv_bootstrap")
+log.setLevel(logging.INFO)
+
+class EscapedHtmlFormatter(logging.Formatter):
+    def __init__(self, fmt, datefmt=None):
+        logging.Formatter.__init__(self, fmt, datefmt)
+
+    def format(self, record):
+        result = logging.Formatter.format(self, record)
+        return cgi.escape(result)
+
+log_handler = logging.StreamHandler()
+log_handler.setFormatter(
+    EscapedHtmlFormatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+)
+log.addHandler(log_handler)
