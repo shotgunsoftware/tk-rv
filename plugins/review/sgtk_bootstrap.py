67aee730a1995e08dfb15c2dbc8aeb1868af7334
Manne Öhrström <manne@shotgunsoftware.com>
2016-09-16 23:40:01 +0100

    updated for plugin id and core 18.26

--- plugins/review/sgtk_bootstrap.py
+++ plugins/review/sgtk_bootstrap.py
@@ -232,7 +232,7 @@ class ToolkitBootstrap(rvt.MinorMode):
             sys.path.append(python_path)
 
             # import manifest that handles config and paths
-            from sgtk_plugin_review import manifest
+            from sgtk_plugin_review_rv import manifest
             log.info("Running Toolkit build %s" % manifest.BUILD_INFO)
 
             # import toolkit core
